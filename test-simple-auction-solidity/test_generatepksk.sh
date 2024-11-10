240da3dbbaeb0d0f98cb5c17d81b3bc6b31f13ff
hashedtitan <183651391+hashedtitan@users.noreply.github.com>
2024-11-10 13:25:02 -0600

    Add troubleshooting test.sh w/ sk & pk

--- test-simple-auction-solidity/test_generatepksk.sh
+++ test-simple-auction-solidity/test_generatepksk.sh
@@ -33,12 +33,17 @@ echo -e "${GREEN}Contract deployed at address: $CONTRACT_ADDRESS${NC}"
 
 sleep 5
 
+# Master public key needs to be generated to encrypt the bid data.
+PUBLIC_KEY_NEW=$(../sharegenerator) # TODO: not sure if there are any inputs needed
+echo -e "${YELLOW}NEW PUBLIC KEY GENERATED: $PUBLIC_KEY_NEW"
+
+
 # User 1 submits a bid using mock bid data from the Rust file
 echo -e "${YELLOW}Submitting encrypted bid from user #1...${NC}"
 
 cd encrypter
 bid_value=100
-Encrypted=$(./encrypter "Random_IBE_ID" $PUBLIC_KEY $bid_value)
+Encrypted=$(./encrypter "Random_IBE_ID" $PUBLIC_KEY_NEW $bid_value)
 cd ..
 BID_DATA=$(python3 convert_to_array.py $Encrypted)
 
@@ -52,7 +57,7 @@ echo -e "${YELLOW}Submitting encrypted bid from user #2...${NC}"
 
 cd encrypter
 bid_value=150
-Encrypted=$(./encrypter "Random_IBE_ID" $PUBLIC_KEY $bid_value)
+Encrypted=$(./encrypter "Random_IBE_ID" $PUBLIC_KEY_NEW $bid_value)
 cd ..
 BID_DATA=$(python3 convert_to_array.py $Encrypted)
 
@@ -66,10 +71,15 @@ sleep 32 #wait at least 2 blocks
 NEW_BLOCK=$(cast block-number --rpc-url $rpc_url)
 echo -e "${YELLOW}New block number: ${NEW_BLOCK}${NC}"
 
-# Reveal the bid using the secret key from the Rust file
+# Testing use of the keyshare from ShareGenerator and formatting it in a way that is needed to test with
+echo -e "${YELLOW}Keyshare obtained from ShareGenerator: $KEYSHARE"
+SECRET_KEY_NEW=$(python3 convert_to_array.py $KEYSHARE)
+echo -e "${YELLOW}Formatted Keyshare obtained from convert_to_array: $SECRET_KEY_NEW"
+
+# TODO: THIS NOW USES THE SECRET_KEY_NEW AND PUBLIC KEY GENERATED FROM SHARE GENERATOR REPO. IT IS NOT WORKING THOUGH.
+# newly generated via share generator repo
 echo -e "${YELLOW}Revealing bid with secret key...${NC}"
-SECRET_KEY="[180,94,231,64,60,139,63,77,251,219,173,163,74,124,6,10,129,139,151,186,102,134,86,99,150,127,59,169,18,212,67,132,48,180,58,172,181,219,30,166,33,104,186,198,23,29,20,141,15,107,179,56,147,33,220,105,191,20,32,206,3,203,206,179,228,207,247,100,37,47,155,29,212,118,240,159,79,249,88,182,208,106,20,154,236,61,92,86,122,253,31,5,161,65,125,200]"
-cast send --rpc-url $rpc_url --private-key $PRIVATE_KEY_1 $CONTRACT_ADDRESS "revealBids(uint8[])" "$SECRET_KEY"
+cast send --rpc-url $rpc_url --private-key $PRIVATE_KEY_1 $CONTRACT_ADDRESS "revealBids(uint8[])" "$SECRET_KEY_NEW"
 echo -e "${GREEN}Bid revealed!${NC}"
 
 sleep 5
